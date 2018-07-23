eb90cda7f126e7c881614d3b837aa166fb97d69e
Taro Matsuzawa <taro@georepublic.co.jp>
2018-07-24 02:43:12 +0900

    Webからクライアントへのデータの受け渡しの実装

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View, WebView } from 'react-native';
 
-// 1: 駅すぱあと路線図のAPIキーをセット
 rosen_api_key = '駅すぱあと路線図のAPIキー'
 
 export default class App extends React.Component {
 
-  // 2: WebViewを読み込んだあとの処理を実装
   loadEnd = () => {
     if (this.webview) {
-      // 3: 初期化を行うために必要な情報を作成
       const data = {
         type: 'init_map',
         api_key: rosen_api_key
       }
-      // 4: postMessageで文字列型にしてWebViewに処理を投げる
       this.webview.postMessage(JSON.stringify(data))
     }
   }
 
+  // 1: メッセージを受け取る関数
+  onMessage = e => {
+    try {
+      // 2: 受け取り側は nativeEvent.data を利用
+      const value = JSON.parse(e.nativeEvent.data)
+      // 3: 確認だけなので console.log で表示
+      if (value.name && value.code) {
+        console.log(value.name, value.code)
+      }
+    } catch (error) {
+      console.log('invalid json data')
+    }
+  }
+
   render() {
-    // 5: refでWebView自体をthis.webviewでアクセス可能にし、onLoadEndで読み込み後の処理を渡す
+    // 4: onMessageでWebView内から受け取ったメッセージを処理する関数を指定
     return (
       <WebView
         source={require('./index.html')}
         ref={webview => {this.webview = webview}}
         onLoadEnd={this.loadEnd}
+        onMessage={this.onMessage}
       />
     )
   }
